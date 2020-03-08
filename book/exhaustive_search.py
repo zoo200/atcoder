44168b166513d0c202b568026ba7861be7fee772
zoo200 <59014257+zoo200@users.noreply.github.com>
2020-03-09 04:18:14 +0900

    Create exhaustive_search.py

---
+++ book/exhaustive_search.py
@@ -0,0 +1,14 @@
+n = int(input())
+a = list(map(int,input().split()))[:n]
+q = int(input())
+m = list(map(int,input().split()))[:q]
+
+def solve(i,m):
+    if m == 0: return True
+    elif i >= n: return False
+    res = solve(i +1 ,m) or solve(i+1,m -a[i])
+    return res
+
+for x in m:
+    if solve(0,x): print('yes')
+    else: print('no')
